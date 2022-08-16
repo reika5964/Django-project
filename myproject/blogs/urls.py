cfcd2eb72c70ba00eb94605f89393b5f7cf57138
Napatsara <nigawa75@gmail.com>
2022-08-16 16:48:41 +0900

    update1-166

--- myproject/blogs/urls.py
+++ myproject/blogs/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index,blogDetail
 
 urlpatterns=[
-    path('',index)
+    path('',index),
+    path('blog/<int:id>',blogDetail,name="blogDetail")
 ]
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blogs'
+    'blogs',
+    'category',
+    'django.contrib.humanize'
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,10 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'myblogs',
+        'USER' :'root',
+        'PASSWORD':''
     }
 }
 
@@ -117,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 

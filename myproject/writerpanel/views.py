6b03fc629fa6f69adbba42eefee715873dfd7938
Napatsara <nigawa75@gmail.com>
2022-08-22 13:54:54 +0900

    finish update data and img

--- myproject/writerpanel/views.py
+++ myproject/writerpanel/views.py
@@ -19,6 +19,7 @@ def panel(request):
     total = Blogs.objects.filter(writer=writer).aggregate(Sum("views"))
     return render(request,"backend/index.html",{"blogs":blogs,"writer":writer,"blogCount":blogCount,"total":total})
 
+@login_required(login_url="member")
 def displayForm(request):
     writer = auth.get_user(request)
     blogs = Blogs.objects.filter(writer=writer)
@@ -27,36 +28,96 @@ def displayForm(request):
     categories =Category.objects.all()
     return render(request,"backend/blogForm.html",{"blogs":blogs,"writer":writer,"blogCount":blogCount,"total":total,"categories":categories})
 
+@login_required(login_url="member")
 def insertData(request):
-    if request.method == "POST" and request.FILES["image"]:
-        datafile = request.FILES["image"]
-        #รับค่าจากฟอร์ม
-        name = request.POST["name"]
-        category = request.POST["category"]
-        description = request.POST["description"]
-        content = request.POST["content"]
-        writer = auth.get_user(request)
+    try:
+        if request.method == "POST" and request.FILES["image"]:
+            datafile = request.FILES["image"]
+            #รับค่าจากฟอร์ม
+            name = request.POST["name"]
+            category = request.POST["category"]
+            description = request.POST["description"]
+            content = request.POST["content"]
+            writer = auth.get_user(request)
 
-        if str(datafile.content_type).startswith("image"):
-            #อัพโหลด
-            fs = FileSystemStorage()
-            img_url ="blogImages/"+datafile.name
-            filename =fs.save(img_url,datafile)
-            #บันทึกข้อมูล
-            blog = Blogs(name =name ,category_id=category,description=description,content=content,writer=writer,image=img_url)
-            blog.save()
-            messages.info(request,"Upload all data are completed")
-            return redirect("displayForm")
-        else:
-            messages.info(request,"This file is not Image. please try again")
-            return redirect("displayForm")
+            if str(datafile.content_type).startswith("image"):
+                #อัพโหลด
+                fs = FileSystemStorage()
+                img_url ="blogImages/"+datafile.name
+                filename =fs.save(img_url,datafile)
+                #บันทึกข้อมูล
+                blog = Blogs(name =name ,category_id=category,description=description,content=content,writer=writer,image=img_url)
+                blog.save()
+                messages.info(request,"Upload all data are completed")
+                return redirect("displayForm")
+            else:
+                messages.info(request,"This file is not Image. please try again")
+                return redirect("displayForm")
 
+    except:   
+        return redirect("displayForm") 
+
+@login_required(login_url="member")
 def deleteData(request,id): 
-    blog = Blogs.objects.get(id=id)
-    fs = FileSystemStorage()
-    fs.delete(str(blog.image))
-    #ลบข้อมูลจากฐานข้อมูล
-    blog.delete()
-    return redirect('panel')
+    try:
+        blog = Blogs.objects.get(id=id)
+        fs = FileSystemStorage()
+        fs.delete(str(blog.image))
+        #ลบข้อมูลจากฐานข้อมูล
+        blog.delete()
+        return redirect('panel')
+    except:
+        return redirect('panel')
 
+@login_required(login_url="member")
+def editData(request,id):
+    #ข้อมูลพื้นฐาน
+    writer = auth.get_user(request)
+    blogs = Blogs.objects.filter(writer=writer)
+    blogCount= blogs.count()
+    total = Blogs.objects.filter(writer=writer).aggregate(Sum("views"))
+    categories =Category.objects.all()
+
+    blogEdit = Blogs.objects.get(id=id)
+    return render(request,"backend/editForm.html",{"blogEdit":blogEdit,"writer":writer,"blogCount":blogCount,"total":total,"categories":categories})
+
+@login_required(login_url="member")        
+def updateData(request,id):
+    try:
+        if request.method == "POST":
+            #ดึงข้อมูลบทความที่ต้องการแก้ไขเดิมมาใช้งาน
+            blog = Blogs.objects.get(id=id)
+            #รับค่าจากฟอร์ม
+            name = request.POST["name"]
+            category = request.POST["category"]
+            description = request.POST["description"]
+            content = request.POST["content"]
+
+            #อัพเดตข้อมูล
+            blog.name = name
+            blog.category_id = category
+            blog.description = description
+            blog.content = content
+            blog.save()
+
+            #อัพเดตภาพปก
+            if request.FILES["image"]:
+                datafile = request.FILES["image"]
+                if str(datafile.content_type).startswith("image"):
+                    #ลบภาพจริงของบทความออกไปก่อน
+                    fs = FileSystemStorage()
+                    fs.delete(str(blog.image))
+
+                    #แทนที่ภาพใหม่
+                    img_url ="blogImages/"+datafile.name
+                    filename =fs.save(img_url,datafile)
+                    blog.image = img_url
+                    blog.save()
+            return redirect("panel")
+    except:
+        return redirect("panel")
+
+
+
+        
         
